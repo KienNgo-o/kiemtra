f4ab29885bbde814c934ea4099ed06f5a3c39530
KienNgo-o <23520799@gm.uit.edu.vn>
2024-11-02 17:35:11 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace DOAN_LAPTRINHMANG
+{
+    partial class Form1
+    {
+
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button_openfile = new System.Windows.Forms.Button();
+            this.button_savefile = new System.Windows.Forms.Button();
+            this.nhap = new System.Windows.Forms.RichTextBox();
+            this.indam = new System.Windows.Forms.Button();
+            this.innghieng = new System.Windows.Forms.Button();
+            this.comboBoxFont = new System.Windows.Forms.ComboBox();
+            this.comboBoxFontSize = new System.Windows.Forms.ComboBox();
+            this.gachchan = new System.Windows.Forms.Button();
+            this.gachbo = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.highlight = new System.Windows.Forms.Button();
+            this.canphai = new System.Windows.Forms.Button();
+            this.cangiua = new System.Windows.Forms.Button();
+            this.cantrai = new System.Windows.Forms.Button();
+            this.kocan = new System.Windows.Forms.Button();
+            this.button_newfile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button_openfile
+            // 
+            this.button_openfile.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_openfile.Location = new System.Drawing.Point(12, 12);
+            this.button_openfile.Name = "button_openfile";
+            this.button_openfile.Size = new System.Drawing.Size(147, 34);
+            this.button_openfile.TabIndex = 0;
+            this.button_openfile.Text = "Open file";
+            this.button_openfile.UseVisualStyleBackColor = true;
+            this.button_openfile.Click += new System.EventHandler(this.button1_docfile);
+            // 
+            // button_savefile
+            // 
+            this.button_savefile.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_savefile.Location = new System.Drawing.Point(12, 52);
+            this.button_savefile.Name = "button_savefile";
+            this.button_savefile.Size = new System.Drawing.Size(127, 34);
+            this.button_savefile.TabIndex = 1;
+            this.button_savefile.Text = "Save As";
+            this.button_savefile.UseVisualStyleBackColor = true;
+            this.button_savefile.Click += new System.EventHandler(this.button_ghifile_Click);
+            // 
+            // nhap
+            // 
+            this.nhap.Location = new System.Drawing.Point(283, 90);
+            this.nhap.Name = "nhap";
+            this.nhap.Size = new System.Drawing.Size(921, 494);
+            this.nhap.TabIndex = 2;
+            this.nhap.Text = "";
+            // 
+            // indam
+            // 
+            this.indam.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.indam.Location = new System.Drawing.Point(184, 12);
+            this.indam.Name = "indam";
+            this.indam.Size = new System.Drawing.Size(38, 34);
+            this.indam.TabIndex = 3;
+            this.indam.Text = "B";
+            this.indam.UseVisualStyleBackColor = true;
+            this.indam.Click += new System.EventHandler(this.indam_click);
+            // 
+            // innghieng
+            // 
+            this.innghieng.Font = new System.Drawing.Font("Bodoni MT", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.innghieng.Location = new System.Drawing.Point(228, 12);
+            this.innghieng.Name = "innghieng";
+            this.innghieng.Size = new System.Drawing.Size(38, 34);
+            this.innghieng.TabIndex = 4;
+            this.innghieng.Text = "I";
+            this.innghieng.UseVisualStyleBackColor = true;
+            this.innghieng.Click += new System.EventHandler(this.innghieng_click);
+            // 
+            // comboBoxFont
+            // 
+            this.comboBoxFont.FormattingEnabled = true;
+            this.comboBoxFont.Location = new System.Drawing.Point(385, 12);
+            this.comboBoxFont.Name = "comboBoxFont";
+            this.comboBoxFont.Size = new System.Drawing.Size(159, 24);
+            this.comboBoxFont.TabIndex = 5;
+            this.comboBoxFont.SelectedIndexChanged += new System.EventHandler(this.comboBoxFont_SelectedIndexChanged);
+            // 
+            // comboBoxFontSize
+            // 
+            this.comboBoxFontSize.FormattingEnabled = true;
+            this.comboBoxFontSize.Location = new System.Drawing.Point(572, 12);
+            this.comboBoxFontSize.Name = "comboBoxFontSize";
+            this.comboBoxFontSize.Size = new System.Drawing.Size(47, 24);
+            this.comboBoxFontSize.TabIndex = 6;
+            this.comboBoxFontSize.SelectedIndexChanged += new System.EventHandler(this.comboBoxFontSize_SelectedIndexChanged);
+            // 
+            // gachchan
+            // 
+            this.gachchan.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gachchan.Location = new System.Drawing.Point(272, 12);
+            this.gachchan.Name = "gachchan";
+            this.gachchan.Size = new System.Drawing.Size(36, 36);
+            this.gachchan.TabIndex = 7;
+            this.gachchan.Text = "U";
+            this.gachchan.UseVisualStyleBackColor = true;
+            this.gachchan.Click += new System.EventHandler(this.gachchan_click);
+            // 
+            // gachbo
+            // 
+            this.gachbo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Strikeout, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gachbo.Location = new System.Drawing.Point(314, 12);
+            this.gachbo.Name = "gachbo";
+            this.gachbo.Size = new System.Drawing.Size(43, 36);
+            this.gachbo.TabIndex = 8;
+            this.gachbo.Text = "ab";
+            this.gachbo.UseVisualStyleBackColor = true;
+            this.gachbo.Click += new System.EventHandler(this.gachbo_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // highlight
+            // 
+            this.highlight.BackgroundImage = global::DOAN_LAPTRINHMANG.Properties.Resources.Screenshot_2024_10_19_1529201;
+            this.highlight.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.highlight.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.highlight.Location = new System.Drawing.Point(385, 52);
+            this.highlight.Name = "highlight";
+            this.highlight.Size = new System.Drawing.Size(33, 32);
+            this.highlight.TabIndex = 13;
+            this.highlight.UseVisualStyleBackColor = true;
+            this.highlight.Click += new System.EventHandler(this.highlight_click);
+            // 
+            // canphai
+            // 
+            this.canphai.BackgroundImage = global::DOAN_LAPTRINHMANG.Properties.Resources.Screenshot_2024_10_19_145849;
+            this.canphai.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.canphai.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.canphai.Location = new System.Drawing.Point(336, 52);
+            this.canphai.Name = "canphai";
+            this.canphai.Size = new System.Drawing.Size(33, 32);
+            this.canphai.TabIndex = 12;
+            this.canphai.UseVisualStyleBackColor = true;
+            this.canphai.Click += new System.EventHandler(this.canphai_click);
+            // 
+            // cangiua
+            // 
+            this.cangiua.BackgroundImage = global::DOAN_LAPTRINHMANG.Properties.Resources.Screenshot_2024_10_19_145135;
+            this.cangiua.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.cangiua.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cangiua.Location = new System.Drawing.Point(231, 52);
+            this.cangiua.Name = "cangiua";
+            this.cangiua.Size = new System.Drawing.Size(33, 32);
+            this.cangiua.TabIndex = 11;
+            this.cangiua.UseVisualStyleBackColor = true;
+            this.cangiua.Click += new System.EventHandler(this.cangiua_click);
+            // 
+            // cantrai
+            // 
+            this.cantrai.BackgroundImage = global::DOAN_LAPTRINHMANG.Properties.Resources.Screenshot_2024_10_19_145216;
+            this.cantrai.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.cantrai.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cantrai.Location = new System.Drawing.Point(283, 52);
+            this.cantrai.Name = "cantrai";
+            this.cantrai.Size = new System.Drawing.Size(33, 32);
+            this.cantrai.TabIndex = 10;
+            this.cantrai.UseVisualStyleBackColor = true;
+            this.cantrai.Click += new System.EventHandler(this.cantrai_click);
+            // 
+            // kocan
+            // 
+            this.kocan.BackgroundImage = global::DOAN_LAPTRINHMANG.Properties.Resources.Screenshot_2024_10_19_144709;
+            this.kocan.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.kocan.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.kocan.Location = new System.Drawing.Point(182, 52);
+            this.kocan.Name = "kocan";
+            this.kocan.Size = new System.Drawing.Size(33, 32);
+            this.kocan.TabIndex = 9;
+            this.kocan.UseVisualStyleBackColor = true;
+            this.kocan.Click += new System.EventHandler(this.kocan_click);
+            // 
+            // button_newfile
+            // 
+            this.button_newfile.Location = new System.Drawing.Point(34, 143);
+            this.button_newfile.Name = "button_newfile";
+            this.button_newfile.Size = new System.Drawing.Size(124, 32);
+            this.button_newfile.TabIndex = 14;
+            this.button_newfile.Text = "New file";
+            this.button_newfile.UseVisualStyleBackColor = true;
+            this.button_newfile.Click += new System.EventHandler(this.button_newfile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(1252, 629);
+            this.Controls.Add(this.button_newfile);
+            this.Controls.Add(this.highlight);
+            this.Controls.Add(this.canphai);
+            this.Controls.Add(this.cangiua);
+            this.Controls.Add(this.cantrai);
+            this.Controls.Add(this.kocan);
+            this.Controls.Add(this.gachbo);
+            this.Controls.Add(this.gachchan);
+            this.Controls.Add(this.comboBoxFontSize);
+            this.Controls.Add(this.comboBoxFont);
+            this.Controls.Add(this.innghieng);
+            this.Controls.Add(this.indam);
+            this.Controls.Add(this.nhap);
+            this.Controls.Add(this.button_savefile);
+            this.Controls.Add(this.button_openfile);
+            this.Name = "Form1";
+            this.Text = "BAI 1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_openfile;
+        private System.Windows.Forms.Button button_savefile;
+        private System.Windows.Forms.RichTextBox nhap;
+        private System.Windows.Forms.Button indam;
+        private System.Windows.Forms.Button innghieng;
+        private System.Windows.Forms.ComboBox comboBoxFont;
+        private System.Windows.Forms.ComboBox comboBoxFontSize;
+        private System.Windows.Forms.Button gachchan;
+        private System.Windows.Forms.Button gachbo;
+        private System.Windows.Forms.Button kocan;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Button cantrai;
+        private System.Windows.Forms.Button cangiua;
+        private System.Windows.Forms.Button canphai;
+        private System.Windows.Forms.Button highlight;
+        private System.Windows.Forms.Button button_newfile;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,332 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Office2016.Drawing.ChartDrawing;
+using Xceed.Words.NET;
+using GemBox.Document;
+namespace DOAN_LAPTRINHMANG
+{
+
+    public partial class Form1 : Form
+    {
+        private const int LinesPerPage = 30;
+        public Form1()
+        {
+            InitializeComponent();
+            ComponentInfo.SetLicense("FREE-LIMITED-KEY");
+            // Chuyển sang chế độ Trial khi đạt giới hạn miễn phí
+            ComponentInfo.FreeLimitReached += (sender, e) => e.FreeLimitReachedAction = FreeLimitReachedAction.ContinueAsTrial;
+
+            nhap.KeyDown += new KeyEventHandler(nhap_KeyDown);
+            nhap.KeyPress += new KeyPressEventHandler(nhap_KeyPress);
+ 
+            // nhap.VScroll += new EventHandler(nhap_VScroll);
+        }
+        /*        private void nhap_VScroll(object sender, EventArgs e)
+                {
+                    UpdatePageNumber();
+                }*/
+        /*        private void UpdatePageNumber()
+                {
+                    int firstVisibleLine = nhap.GetLineFromCharIndex(nhap.GetCharIndexFromPosition(new Point(0, 0)));
+                    int currentPage = (firstVisibleLine / LinesPerPage) + 1;
+                    int totalPages = (nhap.Lines.Length / LinesPerPage) + 1;
+
+                    lblPageNumber.Text = $"Page {currentPage} of {totalPages}";
+                }*/
+        private void button1_docfile(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Word Documents (*.docx)|*.docx";  // Lọc hiển thị file .docx
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = ofd.FileName;
+                if (!string.IsNullOrEmpty(fileName))
+                {
+                    string extension = Path.GetExtension(fileName).ToLower();
+                    if (extension == ".docx")
+                    {
+                        string rtfContent = ConvertDocxToRtf(fileName);
+                        nhap.Rtf = rtfContent;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Vui lòng chọn file văn bản (.docx).");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Bạn chưa chọn file để mở.");
+                }
+            }
+        }
+
+        private string ConvertDocxToRtf(string filePath)
+        {
+            var document = DocumentModel.Load(filePath);
+            using (var stream = new MemoryStream())
+            {
+                document.Save(stream, SaveOptions.RtfDefault);
+                stream.Position = 0;
+                using (var reader = new StreamReader(stream))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+        private void button_ghifile_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Word Document (*.docx)|*.docx";
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = sfd.FileName;
+                if (!string.IsNullOrEmpty(fileName))
+                {
+                    SaveRichTextBoxContentToDocx(fileName);
+                    MessageBox.Show("File đã được lưu thành công!");
+                }
+                else
+                {
+                    MessageBox.Show("Bạn chưa chọn tên tệp để lưu.");
+                }
+            }
+        }
+
+        private void SaveRichTextBoxContentToDocx(string filePath)
+        {
+            // Lấy nội dung RTF từ RichTextBox
+            string rtfContent = nhap.Rtf;
+
+            // Tạo một tài liệu mới
+            var document = new DocumentModel();
+            var section = new Section(document);
+            document.Sections.Add(section);
+
+            // Chèn nội dung RTF vào tài liệu
+            var rtfLoadOptions = new RtfLoadOptions();
+            var rtfDocument = DocumentModel.Load(new MemoryStream(System.Text.Encoding.UTF8.GetBytes(rtfContent)), rtfLoadOptions);
+            foreach (var element in rtfDocument.Sections[0].Blocks)
+            {
+                section.Blocks.Add(document.Import(element, true));
+            }
+
+            // Lưu tài liệu
+            document.Save(filePath);
+        }
+        private void indam_click(object sender, EventArgs e)
+        {
+            if (nhap.SelectionLength > 0)
+            {
+                int selectionStart = nhap.SelectionStart;
+                int selectionLength = nhap.SelectionLength;
+                string selectedFont = comboBoxFont.SelectedItem.ToString();
+
+                for (int i = 0; i < selectionLength; i++)
+                {
+                    nhap.Select(selectionStart + i, 1);
+                    if (nhap.SelectionFont != null)
+                    {
+                        System.Drawing.Font currentFont = nhap.SelectionFont;
+                        System.Drawing.FontStyle newFontStyle = nhap.SelectionFont.Style ^ System.Drawing.FontStyle.Bold;
+                        nhap.SelectionFont = new System.Drawing.Font(currentFont.FontFamily, currentFont.Size, newFontStyle);
+                    }
+                    nhap.Select(selectionStart, selectionLength);
+                }
+            }
+        }
+        private void innghieng_click(object sender, EventArgs e)
+        {
+            if (nhap.SelectionLength > 0)
+            {
+                int selectionStart = nhap.SelectionStart;
+                int selectionLength = nhap.SelectionLength;
+                string selectedFont = comboBoxFont.SelectedItem.ToString();
+
+                for (int i = 0; i < selectionLength; i++)
+                {
+                    nhap.Select(selectionStart + i, 1);
+                    if (nhap.SelectionFont != null)
+                    {
+                        System.Drawing.Font currentFont = nhap.SelectionFont;
+                        System.Drawing.FontStyle newFontStyle = nhap.SelectionFont.Style ^ System.Drawing.FontStyle.Italic;
+                        nhap.SelectionFont = new System.Drawing.Font(currentFont.FontFamily, currentFont.Size, newFontStyle);
+                    }
+                    nhap.Select(selectionStart, selectionLength);
+                }
+            }
+        }
+        private void gachchan_click(object sender, EventArgs e)
+        {
+            if (nhap.SelectionLength > 0)
+            {
+                int selectionStart = nhap.SelectionStart;
+                int selectionLength = nhap.SelectionLength;
+                string selectedFont = comboBoxFont.SelectedItem.ToString();
+
+                for (int i = 0; i < selectionLength; i++)
+                {
+                    nhap.Select(selectionStart + i, 1);
+                    if (nhap.SelectionFont != null)
+                    {
+                        System.Drawing.Font currentFont = nhap.SelectionFont;
+                        System.Drawing.FontStyle newFontStyle = nhap.SelectionFont.Style ^ System.Drawing.FontStyle.Underline;
+                        nhap.SelectionFont = new System.Drawing.Font(currentFont.FontFamily, currentFont.Size, newFontStyle);
+                    }
+                    nhap.Select(selectionStart, selectionLength);
+                }
+            }
+        }
+        private void gachbo_Click(object sender, EventArgs e)
+        {
+            if (nhap.SelectionLength > 0)
+            {
+                int selectionStart = nhap.SelectionStart;
+                int selectionLength = nhap.SelectionLength;
+                string selectedFont = comboBoxFont.SelectedItem.ToString();
+
+                for (int i = 0; i < selectionLength; i++)
+                {
+                    nhap.Select(selectionStart + i, 1);
+                    if (nhap.SelectionFont != null)
+                    {
+                        System.Drawing.Font currentFont = nhap.SelectionFont;
+                        System.Drawing.FontStyle newFontStyle = nhap.SelectionFont.Style ^ System.Drawing.FontStyle.Strikeout;
+                        nhap.SelectionFont = new System.Drawing.Font(currentFont.FontFamily, currentFont.Size, newFontStyle);
+                    }
+                    nhap.Select(selectionStart, selectionLength);
+                }
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Điền danh sách các kiểu chữ vào ComboBox
+            foreach (FontFamily font in FontFamily.Families)
+            {
+                comboBoxFont.Items.Add(font.Name);
+            }
+
+            // Điền danh sách các cỡ chữ vào ComboBox (ví dụ từ 8 đến 72)
+            for (int i = 8; i <= 72; i++)
+            {
+                comboBoxFontSize.Items.Add(i);
+            }
+
+            // Đặt giá trị mặc định cho ComboBox
+            comboBoxFont.SelectedIndex = comboBoxFont.FindString("Arial");
+            comboBoxFontSize.SelectedIndex = comboBoxFontSize.FindString("12");
+        }
+        private void comboBoxFont_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (nhap.SelectionLength > 0)
+            {
+                int selectionStart = nhap.SelectionStart;
+                int selectionLength = nhap.SelectionLength;
+                string selectedFont = comboBoxFont.SelectedItem.ToString();
+
+                for (int i = 0; i < selectionLength; i++)
+                {
+                    nhap.Select(selectionStart + i, 1);
+                    if (nhap.SelectionFont != null)
+                    {
+                        System.Drawing.Font currentFont = nhap.SelectionFont;
+                        nhap.SelectionFont = new System.Drawing.Font(selectedFont, currentFont.Size, currentFont.Style);
+                    }
+                }
+
+                nhap.Select(selectionStart, selectionLength);
+            }
+        }
+
+        private void comboBoxFontSize_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (nhap.SelectionLength > 0)
+            {
+                int selectionStart = nhap.SelectionStart;
+                int selectionLength = nhap.SelectionLength;
+                string selectedFont = comboBoxFont.SelectedItem.ToString();
+
+                for (int i = 0; i < selectionLength; i++)
+                {
+                    nhap.Select(selectionStart + i, 1);
+                    if (nhap.SelectionFont != null)
+                    {
+                        System.Drawing.Font currentFont = nhap.SelectionFont;
+                        float newSize = float.Parse(comboBoxFontSize.SelectedItem.ToString());
+                        nhap.SelectionFont = new System.Drawing.Font(currentFont.FontFamily, newSize, currentFont.Style);
+                    }
+                }
+                nhap.Select(selectionStart, selectionLength);
+            }
+
+        }
+        private void nhap_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Tab)
+            {
+                e.SuppressKeyPress = true;
+                int selectionStart = nhap.SelectionStart;
+                nhap.Text = nhap.Text.Insert(selectionStart, "\t");
+                nhap.SelectionStart = selectionStart + 1;
+            }
+        }
+        private void nhap_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == ' ')
+            {
+                int selectionStart = nhap.SelectionStart;
+                if (selectionStart > 0 && nhap.Text[selectionStart - 1] == '-')
+                {
+                    e.Handled = true;
+                    nhap.Text = nhap.Text.Insert(selectionStart-1, "  ");
+                    nhap.Text = nhap.Text.Insert(selectionStart , "  ");
+                    nhap.SelectionStart = selectionStart + 4;
+                }
+            }
+        }
+        private void kocan_click(object sender, EventArgs e)
+        {
+            nhap.Text= nhap.Text;
+        }
+        private void cangiua_click(object sender, EventArgs e)
+        {
+            nhap.SelectionAlignment = System.Windows.Forms.HorizontalAlignment.Center;
+        }
+
+        private void cantrai_click(object sender, EventArgs e)
+        {
+            nhap.SelectionAlignment = System.Windows.Forms.HorizontalAlignment.Left;
+        }
+
+        private void canphai_click(object sender, EventArgs e)
+        {
+            nhap.SelectionAlignment = System.Windows.Forms.HorizontalAlignment.Right;
+        }
+
+        private void highlight_click(object sender, EventArgs e)
+        {
+            if (nhap.SelectionLength > 0)
+            {
+                nhap.SelectionBackColor = System.Drawing.Color.Yellow; // Màu nền bạn muốn tô
+            }
+        }
+
+        private void button_newfile_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
